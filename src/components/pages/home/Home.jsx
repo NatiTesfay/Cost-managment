75993f39f9102f5a9f75731a72a91db4c1243990
NetanelTesfay <netaneltes66@gmail.com>
2023-02-18 20:32:11 +0200

    1

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -6,8 +6,7 @@ import Card from "react-bootstrap/Card";
   return (
     <Card className="bg-dark text-white">
       <Card.Img
-        src="https://media.istockphoto.com/id/1451864724/photo/man-hand-holding-lightbulb-which-for-mind-creative-idea-innovation-motivation-planning.jpg?s=2048x2048&w=is&k=20&c=IXZoluhWGOUcntTPK9xy3rIEeUeHbbuT7VVEmYzo3t0="
-        alt="Card image"
+       src="/cost_manager.png" alt="image" 
       />
     </Card>
   );
